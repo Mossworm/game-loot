aac02a893cd69336efe323e963ba5e2b44e4162e
Mossworm <55495046+Mossworm@users.noreply.github.com>
2024-01-26 15:33:51 +0900

    fix: https는 https로 통신해야함

--- src/app/steam/ClientContainer.tsx
+++ src/app/steam/ClientContainer.tsx
@@ -21,7 +21,7 @@ export default function ClientContainer() {
                     value="불러오기"
                     className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                     onClick={() => {
-                        fetch('http://steamspy.com/api.php?request=tag&tag=Batman')
+                        fetch('https://steamspy.com/api.php?request=tag&tag=Batman')
                             .then((response) => response.text(), (error) => { console.log(error); })
                             .then((result) => { addText(JSON.stringify(result)); });
                     }}
