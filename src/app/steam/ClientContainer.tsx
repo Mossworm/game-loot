9a68a77a4b7a7faf08e6a41788ee9dac778b846f
Mossworm <55495046+Mossworm@users.noreply.github.com>
2024-01-27 02:26:47 +0900

    feat: 스팀 api 사용중

--- src/app/steam/ClientContainer.tsx
+++ src/app/steam/ClientContainer.tsx
@@ -2,37 +2,24 @@
 import { useState } from "react";
 
 export default function ClientContainer() {
-
-    const [displayText, setDisplayText] = useState('')
-
-    const addText = (text: string) => {
-        setDisplayText(text)
-    }
-
     return (
-        <form>
-            <p>
-                <textarea name="gameTag" placeholder="Please enter the tags" className="block p-2.5 mx-auto text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></textarea>
-            </p>
-            <br />
-            <div className="flex items-center justify-center">
-                <input
-                    type="button"
-                    value="불러오기"
-                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                    onClick={() => {
-                        fetch('https://steamspy.com/api.php?request=tag&tag=Batman')
-                            .then((response) => response.text(), (error) => { console.log(error); })
-                            .then((result) => { addText(JSON.stringify(result)); });
-                    }}
-                />
-            </div>
-            {displayText && (
-                <div>
-                    <br />
-                    <p>{displayText}</p>
+        <div>
+            <form>
+                <p>
+                    <textarea name="gameTag" placeholder="Please enter the tags" className="block p-2.5 mx-auto text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></textarea>
+                </p>
+                <br />
+                <div className="flex items-center justify-center">
+                    <input
+                        type="button"
+                        value="불러오기"
+                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        onClick={() => {
+
+                        }}
+                    />
                 </div>
-            )}
-        </form>
+            </form>
+        </div>
     )
 }
--- src/app/steam/page.tsx
+++ src/app/steam/page.tsx
@@ -1,7 +1,39 @@
+// "use client";
+
 import Link from "next/link";
 import ClientContainer from "./ClientContainer";
 
-export default function page() {
+type GameData = {
+    appid: number;
+    name: string;
+    developer: string;
+    publisher: string;
+    score_rank?: string;
+    positive: number;
+    negative: number;
+    userscore: number;
+    owners: string;
+    average_forever: number;
+    average_2weeks: number;
+    median_forever: number;
+    median_2weeks: number;
+    price: string;
+    initialprice: string;
+    discount: string;
+    ccu: number;
+};
+export default async function page() {
+
+    const targetUrl = 'https://steamspy.com/api.php?request=tag&tag=City Builder';
+
+    const resp = await fetch(targetUrl);
+    const data = await resp.json();
+
+    // 확인을 위해 데이터 출력
+    console.log(data);
+
+    const apps: GameData[] = Object.values(data);
+
     return (
         <div className="flex items-center justify-center h-screen">
             <div>
@@ -15,7 +47,15 @@ export default function page() {
                 <br />
                 띄어쓰기없이 쉼표로 구분하여 입력해주세요.
                 <ClientContainer />
+                <br />
+                <div>
+                    <ul>
+                        {apps.slice(0, 10).map((game) => (
+                            <li key={game.appid}>{game.name}</li>
+                        ))}
+                    </ul>
+                </div>
             </div>
         </div>
-    )
+    );
 }
