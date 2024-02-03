33b50a4b34e348ff193e470fe487b2fd618c9658
Mossworm <55495046+Mossworm@users.noreply.github.com>
2024-02-03 22:08:26 +0900

    fix: 폴더 경로 변경

--- src/app/survey/page.tsx
+++ src/app/survey/page.tsx
@@ -2,19 +2,20 @@
 // 그냥 단일 페이지에서 어떻게든 페이징을 구현할 거임.
 // 다음 버튼은 모든 문항이 응답되었는지를 확인하는 기능을 추가할 수 있음.
 // 마지막의 다음 버튼은 마지막 페이지일 때 결과 버튼으로 바뀜.
+"use client"
 
-import RadioBtns from "../components/RadioBtns"
+import RadioBtns from "@/components/RadioBtns"
 
 const options = [
-    {label: '옵션1', value: '옵션1'},
-    {label: '옵션2', value: '옵션2'},
+    { label: '옵션1', value: '옵션1' },
+    { label: '옵션2', value: '옵션2' },
 ];
 
 export default function SurveyPage() {
     return (
         <div>
             첫번째 페이지
-            <RadioBtns options={options} onSelect={()=>{}}/>
+            <RadioBtns options={options} onSelect={() => { }} />
         </div>
     )
 }
