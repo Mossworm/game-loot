d4ea6aa49990547790ebcf702e16f072312b8e70
34-43 <121276581+34-43@users.noreply.github.com>
2024-03-04 05:46:04 +0900

    feat: '/games' api query update

---
+++ src/pages/api/games.ts
@@ -0,0 +1,37 @@
+import { NextApiRequest, NextApiResponse } from "next"
+import clientPromise from "@/lib/mongodb";
+import { GameData } from "@/lib/interface";
+
+const coll_service_name = "FinalApps";
+const coll_table_name = "PermutationTable";
+
+export default async (req:NextApiRequest, res:NextApiResponse) => {
+    try {
+        const questionIndexQuery = req.query.index
+        const client = await clientPromise;
+        const db = client.db("GameLoot");
+        const coll_service = db.collection(coll_service_name);
+        const coll_table = db.collection(coll_table_name)
+
+        const p_table : Array<number> = (await coll_table.findOne())?.table;
+
+        if (questionIndexQuery && typeof(questionIndexQuery) === "string") {
+            //find one
+            const i = parseInt(questionIndexQuery);
+            const findOne = await coll_service.findOne({index:p_table[i]});
+            res.json(findOne);
+        } else {
+            //find all
+            const dataArray = new Array<any>;
+            for (let i = 0; i < 80; i++) {
+                const findOne = await coll_service.findOne({index:p_table[i]});
+                if (findOne) dataArray[i] = findOne;
+            }
+            res.json(dataArray);
+        }
+        
+    } catch (e) {
+        console.error(e);
+        res.end(`error : ${e}`);
+    }
+}
